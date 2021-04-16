363eb37c0e37a5b3104bf4d560bb34d7b345248d
cremirdevio <josephajibodu@gmail.com>
2021-04-16 11:48:29 +0100

    Gtag added to the authentication pages

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-TDKQZXZ');</script>
 <!-- End Google Tag Manager -->
+
   <meta charset="utf-8">
   <meta name="author" content="Softnio">
   <meta name="csrf-token" content="{{ csrf_token() }}">
