22794981675d48eb88e3ab8c4249b6ca48822933
cremirdevio <josephajibodu@gmail.com>
2021-04-27 05:05:04 -0700

    👏 Handling old implementations for transaction images

--- resources/views/admin/transactions/show.blade.php
+++ resources/views/admin/transactions/show.blade.php
@@ -146,12 +146,17 @@
               <li>
                 <div class="data-details-head">Image Uploads</div>
                 <div class="data-details-des d-flex justify-content-start">
-                  @forelse(json_decode($transaction->images) as $image)
-                  <a href="{!! Storage::url('gift-cards/'.$image) !!}" class="img-thumbnail mr-2" target="_blank"><img
-                      src="{!! Storage::url('gift-cards/'.$image) !!}" height="30px" alt=""></a>
-                  @empty
-                  <span class="text-primary">No Image uploaded! <i class="fa fa-times text-danger"></i> </span>
-                  @endforelse
+                  @if(!is_null($transaction->images))
+                    @forelse(json_decode($transaction->images) as $image)
+                      <a href="{!! Storage::url('gift-cards/'.$image) !!}" class="img-thumbnail mr-2" target="_blank"><img
+                          src="{!! Storage::url('gift-cards/'.$image) !!}" height="30px" alt=""></a>
+                    @empty
+                      <span class="text-primary">No Image uploaded! <i class="fa fa-times text-danger"></i> </span>
+                    @endforelse
+                  @else
+                  <!-- This handles old implementations where images is NULL rather than the recenet [] -->
+                    <span class="text-primary">No Image uploaded! <i class="fa fa-times text-danger"></i> </span>
+                  @endif
                 </div>
               </li>
               @endif
