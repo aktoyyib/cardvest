64d09db383e628e0e598b4b19a2838e18d686f76
cremirdevio <josephajibodu@gmail.com>
2021-05-21 09:18:04 +0100

    ✨ Admin Roles functionalities completed;

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -86,6 +86,22 @@
           </div>
         </div>
       </div><!-- .col -->
+
+      <div class="col-lg-4">
+        <div class="token-statistics card card-token height-auto">
+          <div class="card-innr">
+            <div class="token-balance token-balance-with-icon">
+              <div class="token-balance-icon">
+                <img src="{{ asset('images/logo-sm.png') }}" alt="logo">
+              </div>
+              <div class="token-balance-text">
+                <span class="lead"><span>Administrators</span></span>
+                <h6 class="card-sub-title"><a href="{{ route('roles.index') }}">View</a></h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><!-- .col -->
     </div>
 
   </div><!-- .container -->
