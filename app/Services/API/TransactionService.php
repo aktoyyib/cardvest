1c287391947d8815d4740d721c28cac66c101b83
cremirdevio <josephajibodu@gmail.com>
2021-06-05 10:47:39 +0100

    Webhook processing charge handling

--- app/Services/API/TransactionService.php
+++ app/Services/API/TransactionService.php
@@ -319,7 +319,7 @@ class TransactionService
     protected function processCharge($data) {
         Log::info(request()->data['tx_ref']);
         // Get the transaction from your DB using the transaction reference (txref)
-        $transaction = Transaction::where('reference', request->data['tx_ref'])->first();
+        $transaction = Transaction::where('reference', request()->data['tx_ref'])->first();
         
         // Check if you have previously given value for the transaction. If you have, redirect to your successpage else, continue
         if ($transaction->payment_status === 'succeed' && $transaction->status === 'succeed') exit();
