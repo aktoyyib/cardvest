b43d23ad0094406d971c59278e02745bf74233f2
cremirdevio <josephajibodu@gmail.com>
2021-10-21 18:31:11 +0100

    GI - Payment bindings and resolution
    Theteller base class(Payswitch) is binded to the service provider as payswitch
    The service provider is then added to the list of providers
    A Facade is created on top to the payswitch binding
    The facade is registered in the alias section of the ./config/app.php
    Configurations needed by payswitch is added to ./config/theteller.php
    :

---
+++ app/Providers/ThetellerServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\Payment\PaySwitch;
+use Illuminate\Support\ServiceProvider;
+
+class ThetellerServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('payswitch', function ($app) {
+            return new PaySwitch();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
