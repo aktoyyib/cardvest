1896b24c3bc82c173b801868f272c82bddc51160
cremirdevio <josephajibodu@gmail.com>
2021-10-21 13:13:49 +0100

    Ghana Integration UI
    Wallet widget UI modified

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,9 +15,35 @@
                 <img src="images/logo-sm.png" alt="logo">
               </div>
               <div class="token-balance-text">
-                <h6 class="card-sub-title">Wallet Balance</h6>
+                <h6 class="card-sub-title">Main Balance</h6>
                 <span class="lead">{{ to_naira($user->balance()) }} <span>NGN</span></span>
               </div>
+              <div class="token-pay-currency border-0">
+                <small>Default Currency</small>
+                <a href="#" class="link ucap link-light toggle-tigger toggle-caret text-white">NGN</a>
+                <div class="toggle-class dropdown-content" style="right: -20%; left: unset;">
+                    <ul class="dropdown-list">
+                        <li><a href="#">BTC</a></li>
+                        <li><a href="#">LTC</a></li>
+                        <li><a href="#">USD</a></li>
+                    </ul>
+                </div>
+              </div>
+            </div>
+
+            <div class="token-balance token-balance-s2">
+              <hr class="bg-warning">
+              <!-- <h6 class="card-sub-title">Your Wallets</h6> -->
+              <ul class="token-balance-list justify-content-between">
+                  <li class="token-balance-sub">
+                      <span class="lead">2,646</span>
+                      <span class="sub"><a href="/">&#8373; (CEDIS) <i class="fa fa-external-link"></i></a> </span>
+                  </li>
+                  <li class="token-balance-sub">
+                      <span class="lead">1,265</span>
+                      <span class="sub"><a href="/">&#8358; (NAIRA) <i class="fa fa-external-link"></i></a> </span>
+                  </li>
+              </ul>
             </div>
           </div>
         </div>
