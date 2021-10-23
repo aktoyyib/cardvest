053c23b6f425d445954995d85f46cc15ad370517
cremirdevio <josephajibodu@gmail.com>
2021-10-23 16:15:52 +0100

    GI - PaymentGateway implementation updated to allow for custom currency

--- app/Payment/PaymentGateway.php
+++ app/Payment/PaymentGateway.php
@@ -8,6 +8,9 @@ use App\Payment\ThetellerPayment;
 
 class PaymentGateway
 {
+    // Calls a specified function with arguement on the payment
+    // class decided by the users default currency
+    // This should be used sparingly
     public static function __callStatic($method, $arguments)
     {
         $currency = auth()->user()->currency();
@@ -17,6 +20,20 @@ class PaymentGateway
         }
         if ($currency == config('currency.list.ghana')) {
             return (new ThetellerPayment())->$method(...$arguments);
+        }
+
+        throw new \Exception('Invalid currency processing');
+    }
+
+    // Return the proper payment class based on a set currency
+    // Recommended 
+    public static function currency(string $currency)
+    {
+        if ($currency == config('currency.list.nigeria')) {
+            return (new FlutterPayment());
+        }
+        if ($currency == config('currency.list.ghana')) {
+            return (new ThetellerPayment());
         }
 
         throw new \Exception('Invalid currency processing');
