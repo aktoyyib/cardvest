4debd6266006aa45a4baa40250107f5ecc1cec02
cremirdevio <josephajibodu@gmail.com>
2021-10-23 16:01:02 +0100

    GI - Class 'App\Services\Payment\PaySwitch' not found resolved

--- app/Providers/ThetellerServiceProvider.php
+++ app/Providers/ThetellerServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use App\Services\Payment\PaySwitch;
+use App\Services\Payment\Payswitch;
 use Illuminate\Support\ServiceProvider;
 
 class ThetellerServiceProvider extends ServiceProvider
