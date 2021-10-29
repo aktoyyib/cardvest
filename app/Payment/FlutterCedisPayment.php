ca4086454c82e8187aad700bdd413f1b70312300
cremirdevio <josephajibodu@gmail.com>
2021-10-30 00:29:31 +0100

    GI - Class for handling Cedis payment created
    PaymentGatewaty class to use Cedis Payment dynamically -
    with Flutter Naira payment.

---
+++ app/Payment/FlutterCedisPayment.php
@@ -0,0 +1,148 @@
+<?php
+
+
+namespace App\Payment;
+
+
+use App\Facades\Theteller;
+use App\Models\Withdrawal;
+use App\Models\Transaction;
+use KingFlamez\Rave\Facades\Rave as Flutterwave;
+
+class FlutterCedisPayment implements Payment
+{
+
+    public function withdraw(\App\Models\Withdrawal $withdrawal): array
+    {
+        $reference = $withdrawal->reference;
+        $bank = $withdrawal->bank;
+        $user = $withdrawal->user;
+
+        try {
+            // Make transfer here with flutterwave api
+            $data = [
+                "account_bank" => $bank->code, //MTN, TIGO, VODAFONE, AIRTEL | GH280100
+                "account_number" => $bank->banknumber, //233xxxxxxxxxx | 0031625807099
+                "amount" => $withdrawal->amount,
+                "currency" => $withdrawal->currency,
+                "beneficiary_name" => $bank->accountname,
+                // "destination_branch_code" => "GH280103", // For Bank transfers
+                "narration" => "Cardvest - Funds withdrawal " . $reference,
+                "reference" => $reference,
+            ];
+
+            // Check if its mobile money or bank transfer
+            if (!is_numeric($bank->code)) {
+                $data['destination_branch_code'] = $bank->branch_code;
+            }
+
+            // Send the money to the user
+            $transfer = Flutterwave::transfers()->initiate($data);
+            return $transfer;
+        } catch (\Throwable $e) {
+            throw $e;
+        }
+    }
+
+    public function makePayment(Transaction $transaction, string $callback_url): string
+    {
+        // Needed: transaction, card, amount, user, reference, callback_url
+        $user = $transaction->user;
+        $card = $transaction->card;
+
+        try {
+            // Initialize Payment
+            // Enter the details of the payment
+            $data = [
+                'amount' => $transaction->amount/100,
+                'email' => $user->email,
+                'tx_ref' => $transaction->reference,
+                'currency' => $user->currency(),
+                'redirect_url' => $callback_url,
+                'customer' => [
+                    'email' => $user->email,
+                    "phone_number" => $user->phonenumber,
+                    "name" => $user->username
+                ],
+
+                "customizations" => [
+                    "title" => "Cardvest",
+                    "description" => "Buy $".$transaction->unit." ".$card->name." at ".$card->rate."/$",
+                    "logo" => asset('images/logo-sm.png')
+                ],
+            ];
+
+            $payment = Flutterwave::payments()->momoGH($data);
+
+
+            if ($payment['status'] !== 'success') {
+                // notify something went wrong
+                abort(500, 'Payment could not be initialised');
+            }
+
+            return $payment['data']['link'];
+
+        } catch (\Throwable $e) {
+            throw $e;
+        }
+
+    }
+
+    public function verifyTransaction(string $transactionID = null)
+    {
+        $transactionID = is_null($transactionID) ? Flutterwave::getTransactionIDFromCallback() : $transactionID;
+        $data = Flutterwave::verifyTransaction($transactionID);
+        return $data['data'];
+    }
+
+    public function verifyWebHook(): bool
+    {
+        return Flutterwave::verifyWebhook();
+    }
+
+    public function fetchBanks(): array
+    {
+        $banks = Flutterwave::banks()->ghana();
+        // Filter out normal banks and leave mobile banks
+        $mobileBanks = array_filter($banks, function($bank) {
+            return !is_numeric($bank->code);
+        });
+
+        return $mobileBanks;
+    }
+
+    public function resolveAccount(string $accountnumber, string $bank)
+    {}
+
+    public function verifyTransfer(string $id): array
+    {
+        $transfer = Flutterwave::transfers()->fetch($id);
+        return $transfer;
+    }
+
+    public function generateReference(): string
+    {
+        return Flutterwave::generateReference();
+    }
+
+    public function getReferenceFromCallback(): string
+    {
+        $reference = request()->tx_ref;
+        return $reference;
+    }
+
+    public function isSuccessfulAndValid($data, Transaction $transaction): bool
+    {
+        if ($data['status']  !== 'successful') {
+            return false;
+        }
+
+        // Confirm that the db transaction amount is equal to the returned amount
+        $amt = $transaction->amount/100;
+        if ($data['amount']  !== $amt) {
+            return false;
+        }
+
+        return true;
+    }
+}
