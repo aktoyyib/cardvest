1fc698c7329673c6ba91df25759e723b4d3ba38f
cremirdevio <josephajibodu@gmail.com>
2021-11-09 23:39:58 +0100

    Quick Fix for generating admin

--- app/Console/Commands/MakeUserAdmin.php
+++ app/Console/Commands/MakeUserAdmin.php
@@ -13,7 +13,7 @@ class MakeUserAdmin extends Command
      *
      * @var string
      */
-    protected $signature = 'admin:add {email : The email address of the user}';
+    protected $signature = 'admin:add {email : The email address of the user} {--super}';
 
     /**
      * The console command description.
@@ -41,24 +41,44 @@ class MakeUserAdmin extends Command
     {
         $email = $this->argument('email');
         $pass = $this->secret('Enter passphrase for super admin access');
+        $super = $this->option('super');
 
-
-        while ($pass !== 'pAsSwOrD') {
-            $this->error('Wrong passphrase!');
-            $pass = $this->secret('Enter passphrase for super admin access');
-        }
+        // while ($pass !== 'pAsSwOrD') {
+        //     $this->error('Wrong passphrase!');
+        //     $pass = $this->secret('Enter passphrase for super admin access');
+        // }
 
         $role = Role::where('name', 'admin')->first();
+        $superAdmin = Role::where('name', 'super admin')->first();
         $user = User::where('email', $email)->first();
 
         if (is_null($user)) {
             return $this->error('User with specified email address does not exist.');
         }
 
+        $responses = array();
+
         if ($user->hasRole(['admin'])) { //hasAnyRoles([])
-            return $this->info('User is already an admin.');
+            $responses[] = "User is already an admin!";
+        } else {
+            $user->assignRole($role);
         }
-        $user->assignRole($role);
+
+        if ($super) {
+            if ($user->hasRole(['super admin'])) {
+                $responses[] = "User is already a super admin!";
+            } else {
+                $user->assignRole($superAdmin);
+            }
+        }
+
+        if (count($responses) > 0) {
+            foreach ($responses as $response) {
+                $this->error($response);
+            }
+
+            return 0;
+        }
 
         $this->info("User (".$email.") now has admin priviledges");
     }
